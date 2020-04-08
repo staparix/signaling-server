31d260d0b8914582b0709b814d80f7f266cac36b
abeinarovics <a.beinarovics@gmail.com>
2020-04-08 17:53:21 +0300

    fix ws server

--- src/signaling-server.ts
+++ src/signaling-server.ts
@@ -1,17 +1,32 @@
-import * as WebSocket from "ws";
-// @ts-ignore
-const PORT = process.env.PORT ?? 3000;
 
-const wss = new WebSocket.Server({ port: PORT });
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
         console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
     });
 
-    ws.send('something');
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server from heroku');
 });
 
-setInterval(() => {
-    wss.emit(Date.now())
-}, 2000);
+//start our server
+const PORT = process.env.PORT || 8999
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} :)`);
+});
